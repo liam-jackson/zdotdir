405965328a532bb309326f1b7a4f2569e2376f0b
Liam Jackson <liamjackson126@gmail.com>
2025-04-14 22:48:04 -0400

    zvm vim mode colors
    * p10k vi mode indicator tweaks to match zsh vim mode plugin (current
    state, not perfect)

--- .zshrc.d/zvm-init.zsh
+++ .zshrc.d/zvm-init.zsh
@@ -1,49 +1,51 @@
 # Set vi-mode:
 export ZVM_KEYTIMEOUT=0.1
-# export ZVM_LAZY_KEYBINDINGS=false
-
-# Do the initialization when the script is sourced (i.e. Initialize instantly)
-# ZVM_INIT_MODE=sourcing
 
 # The plugin will auto execute this zvm_config function
 function zvm_config() {
   # Retrieve default cursor styles
   local icur=$(zvm_cursor_style $ZVM_CURSOR_BLINKING_BEAM)
   local ncur=$(zvm_cursor_style $ZVM_CURSOR_BLOCK)
+  local ocur=$(zvm_cursor_style $ZVM_CURSOR_UNDERLINE)
 
   # Append your custom color for your cursor
-  ZVM_INSERT_MODE_CURSOR=$icur'\e\e]12;#aa80ff\a'
-  ZVM_NORMAL_MODE_CURSOR=$ncur'\e\e]12;#008800\a'
+  ZVM_INSERT_MODE_CURSOR=$icur'\e\e]12;#2222ff\a'
+  ZVM_NORMAL_MODE_CURSOR=$ncur'\e\e]12;#00ff00\a'
+  ZVM_OPPEND_MODE_CURSOR=$ocur'\e\e]12;#ff0000\a'
 
   # ZVM_VI_HIGHLIGHT_FOREGROUND=#008800        # Hex value
   # ZVM_VI_HIGHLIGHT_BACKGROUND=#ff0000        # Hex value
-  ZVM_VI_HIGHLIGHT_EXTRASTYLE='underline' # bold and underline
+  # ZVM_VI_HIGHLIGHT_EXTRASTYLE='underline' # bold and underline
 
   # Always starting with insert mode for each command line
   ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
-
 }
+
+# zvm_before_init_commands=()
+# zvm_after_init_commands=()
 
 # The plugin will auto execute this zvm_after_init function
 function zvm_after_init() {
   _fzf_source
 }
 
-# Append a command directly
-# zvm_after_init_commands+=(_fzf_source)
+# zvm_before_select_vi_mode_commands=()
+# zvm_after_select_vi_mode_commands=()
+
+# zvm_before_lazy_keybindings_commands=()
 
 # The plugin will auto execute this zvm_after_lazy_keybindings function
 function zvm_after_lazy_keybindings() {
   [[ -f "${ZDOTDIR}/.keybinds" ]] && source "${ZDOTDIR}/.keybinds"
 
-  # Here we define the custom widget
-  # zvm_define_widget atuin-search
-
   # In normal mode, press Ctrl-r to invoke this widget
   zvm_bindkey vicmd '^r' atuin-search
   zvm_bindkey viins '^r' atuin-search
   zvm_bindkey viins '^I' fzf-tab-complete
-  zvm_bindkey viins '^A' fzf-help-widget
+  # zvm_bindkey viins '^XA' fzf-help-widget
+  # zvm_bindkey viins '^ ' fzf-man-widget
+  zvm_bindkey viins '^[' zvm_exit_insert_mode
+  zvm_bindkey viins '\e.' append-last-word
 }
 
-source "$(antidote path jeffreytse/zsh-vi-mode)/zsh-vi-mode.plugin.zsh"
+source "$(antidote path jeffreytse/zsh-vi-mode)/zsh-vi-mode.zsh"
